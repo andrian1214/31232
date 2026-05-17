74be9ed86a159c4e92bf7ca61176f0b898ed750b
andrian1214 <andrianrian268@gmail.com>
2026-05-17 23:41:57 +0700

    Update Dashboard.py

--- Dashboard 3/Dashboard.py
+++ Dashboard 3/Dashboard.py
@@ -3,9 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly.express as px
-import plotly.graph_objects as go
-import os
 
 # --- KONFIGURASI HALAMAN ---
 st.set_page_config(
